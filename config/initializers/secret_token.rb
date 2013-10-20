9aaf6e820f72f7df130e5b09adf505f040df58dd
Zachary Drummond <zdrummond@biaprotect.com>
2013-10-20 11:35:23 -0700

    start

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Shadowrun::Application.config.secret_key_base = 'e0053bea91e5ab32cc3847e264614dd78d631eb83ca9716d32e55ad6266c6f182fccf1edd2fa16fe1629b67eb7a0a6cbc89cbee52c04478a402db04a9110651d'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Shadowrun::Application.config.session_store :cookie_store, key: '_shadowrun_session'
